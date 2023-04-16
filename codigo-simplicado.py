420f571c7790db448f45b47cacddf57a8608a51e
NEBULA1978 <rpascualey@gmail.com>
2023-04-16 12:26:50 +0200

    Resumen de archivo codigo.py en archivo codigo-simplificado.py

---
+++ codigo-simplicado.py
@@ -0,0 +1,132 @@
+# print("Hola\nmi nombre es Ramon")
+# print("mi nombre es Ramon")
+
+# Declaración de variables y tipos de datos
+
+# numero =10
+# numero =10.56
+# print(Hola mundo)
+# print(numero)
+# print(type(numero))
+
+# cadena = 'a'
+# cadena = "Estoy'estudiando'"
+# cadena = 'Estoy"estudiando"'
+
+# print(cadena)
+# print(type(cadena))
+
+# valor = True
+# valor = False
+# print(valor)
+# print(type(valor))
+
+# num1 = 10
+# num2 = 6.7
+# suma = num1+num2 * 10 /6
+# suma = (num1+num2) * 10 /6
+# resultado = (num1+num2) * 10 /6
+
+# print("El resultado es: ",resultado)
+# print(suma)
+# print(type(suma))
+# print(suma)
+# print(suma)
+
+# El tipado dinámico permite que una variable pueda contener diferentes tipos de datos a lo largo del programa.
+
+# valor = 10
+# print(valor)
+# valor = "Ramon"
+# print(valor)
+
+# Operadores aritméticos
+
+# resultado = 10 +5
+
+# num1 = 2
+# num2 = 5
+
+# resultado = num1 + num2
+# resultado = num1 - num2
+# resultado = num1 * num2
+# resultado = num1 / num2
+# resultado = num1 // num2
+# resultado = num1 % num2
+# resultado = num1 ** num2
+# resultado = 3**3 * (13/5-(2*4))
+
+# print(resultado)
+
+# Operadores relacionales
+
+# resultado = 10 > 20
+# resultado = 10 != 20
+# resultado = 10 <= 10
+# resultado = 10 >= 10
+
+# print(resultado)
+
+# a = 10
+# b = 20
+# c = 30
+
+# resultado = ((a>b)) and ((b<c))
+# resultado = ((a>b)) or ((b<c))
+# resultado = not((a>b)) or ((b<c))
+# print(resultado)
+
+# Operadores lógicos
+
+# and conjunción
+# or disyunción
+# not negación
+
+# a = 10
+# b = 15
+# c = 20
+# resultado = ((a>b)) and ((b<c))
+# resultado = ((a>b)) or ((b<c))
+# resultado = not((a>b)) or ((b<c))
+# print(resultado)
+
+# Operadores de asignación
+
+# a = 0
+# a += 5
+# a -= 2
+# a *= 3
+# a /= 3
+# a **=2
+# a %=2
+
+# print(a)
+
+# Salidas de datos
+
+# nombre ="Ramon"
+# edad = 22
+
+# print("Hola",nombre,"tienes",edad,"años")
+# print("Hola {} tienes {} años".format(nombre,edad))
+# print(f"Hola {nombre} tienes {edad} años")
+
+# Entrada de datos
+
+# nombre = input("Digite su nombre: ")
+# print(f"Hola {nombre}")
+# numero= int(input("Digite su numero: "))
+# print(f"El numero es {numero+1}")
+
+# Funciones integradas
+
+n = int("10")  # convierte una cadena en un entero
+n = float("10.8")  # convierte una cadena en un flotante
+n = str(10.98)  # convierte un número en una cadena de texto
+n = bin(10)  # convierte un número entero en binario
+n = hex(10)  # convierte un número entero en hexadecimal
+n = int("0b1010", 2)  # convierte un número binario en entero
+n = int("0xa", 16)  # convierte un número hexadecimal en entero
+n = abs(-8)  # devuelve el valor absoluto de un número
+n = round(5.4)  # redondea un número
+n = len("Ramon")  # devuelve la longitud de una cadena
